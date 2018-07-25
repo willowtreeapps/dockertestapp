10e1b05342809a96b4dd2a8cf83d3d77aeefd8ef
Konrad Winkler <konrad.winkler@willowtreeapps.com>
2018-07-25 14:43:35 -0400

    first espresso test

--- app/src/androidTest/java/com/example/konradwinkler/docker_test_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/konradwinkler/docker_test_app/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.konradwinkler.docker_test_app
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,18 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val activity =  ActivityTestRule<MainActivity>(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.example.konradwinkler.docker_test_app", appContext.packageName)
+
+        Espresso.onView(ViewMatchers.withId(R.id.button_click_me)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.check_me)).check(ViewAssertions.matches(ViewMatchers.isChecked()))
     }
 }
--- app/src/main/java/com/example/konradwinkler/docker_test_app/MainActivity.kt
+++ app/src/main/java/com/example/konradwinkler/docker_test_app/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.konradwinkler.docker_test_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_click_me.setOnClickListener {
+            Log.d("GENERAL", "clicked the button")
+            check_me.isChecked = !check_me.isChecked
+        }
     }
+
 }
